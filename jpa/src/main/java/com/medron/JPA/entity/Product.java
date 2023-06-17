3bdbde736005b048043870ce411a346397ba66e6
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2023-06-17 16:51:37 +0300

    Jpa #5

--- jpa/src/main/java/com/medron/JPA/entity/Product.java
+++ jpa/src/main/java/com/medron/JPA/entity/Product.java
@@ -1,15 +1,14 @@
 package com.medron.jpa.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
+@ToString
+@Builder
 @Setter
 @Table(name = "products",uniqueConstraints = {@UniqueConstraint(name = "productName",columnNames = "name")})
 public class Product {
--- jpa/src/main/java/com/medron/JPA/repository/ProductRepository.java
+++ jpa/src/main/java/com/medron/JPA/repository/ProductRepository.java
@@ -4,7 +4,10 @@ import com.medron.jpa.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Integer> {
+    Optional<Product> findByName(String name);
 }
